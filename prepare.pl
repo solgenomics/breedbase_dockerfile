47be69db5fafa018b8e21137bb305bdf5a0988f1
Lukas Mueller <lam87@cornell.edu>
2019-06-07 11:12:17 -0400

    add a prepare script that clones repos into current dir

---
+++ prepare.pl
@@ -0,0 +1,37 @@
+
+# main code
+git clone --quiet https://github.com/solgenomics/cxgn-corelibs.git /home/production/cxgn/cxgn-corelibs
+git clone --quiet https://github.com/solgenomics/sgn.git /home/production/cxgn/sgn
+git clone --quiet https://github.com/solgenomics/Phenome.git /home/production/cxgn/Phenome
+git clone --quiet https://github.com/solgenomics/rPackages.git /home/production/cxgn/rPackages
+git clone --quiet https://github.com/solgenomics/biosource.git /home/production/cxgn/biosource
+git clone --quiet https://github.com/solgenomics/Cview.git /home/production/cxgn/Cview
+git clone --quiet https://github.com/solgenomics/ITAG.git /home/production/cxgn/ITAG
+git clone --quiet https://github.com/solgenomics/tomato_genome.git /home/production/cxgn/tomato_genome
+git clone --quiet https://github.com/GMOD/Chado.git /home/production/cxgn/Chado
+git clone --quiet https://github.com/solgenomics/sgn-devtools.git /home/production/cxgn/sgn-devtools
+git clone --quiet https://github.com/solgenomics/solGS.git /home/production/cxgn/solGS
+git clone --quiet https://github.com/solgenomics/Barcode-Code128.git /home/production/cxgn/Barcode-Code128
+git clone --quiet https://github.com/solgenomics/starmachine.git /home/production/cxgn/starmachine
+
+# Mason website skins
+git clone --quiet https://github.com/solgenomics/cassava.git /home/production/cxgn/cassava
+git clone --quiet https://github.com/solgenomics/yambase.git /home/production/cxgn/yambase
+git clone --quiet https://github.com/solgenomics/sweetpotatobase.git /home/production/cxgn/sweetpotatobase
+git clone --quiet https://github.com/solgenomics/ricebase.git /home/production/cxgn/ricebase
+git clone --quiet https://github.com/solgenomics/citrusgreening.git /home/production/cxgn/citrusgreening
+git clone --quiet https://github.com/solgenomics/coconut.git /home/production/cxgn/coconut
+git clone --quiet https://github.com/solgenomics/cassbase.git /home/production/cxgn/cassbase
+git clone --quiet https://github.com/solgenomics/musabase.git /home/production/cxgn/musabase
+git clone --quiet https://github.com/solgenomics/potatobase.git /home/production/cxgn/potatobase
+git clone --quiet https://github.com/solgenomics/cea.git /home/production/cxgn/cea
+git clone --quiet https://github.com/solgenomics/cippotatobase.git /home/production/cxgn/cippotatobase
+git clone --quiet https://github.com/solgenomics/fernbase.git /home/production/cxgn/fernbase
+git clone --quiet https://github.com/solgenomics/solgenomics.git /home/production/cxgn/solgenomics
+git clone --quiet https://github.com/solgenomics/panzeabase.git /home/production/cxgn/panzeabase
+git clone --quiet https://github.com/solgenomics/varitome.git /home/production/cxgn/varitome
+git clone --quiet https://github.com/solgenomics/milkweed.git /home/production/cxgn/milkweed
+#git clone --quiet https://github.com/solgenomics/erysimum.git /home/production/cxgn/erysimum
+git clone --quiet https://github.com/solgenomics/vitisbase.git /home/production/cxgn/vitisbase
+git clone --quiet https://github.com/solgenomics/panandbase.git /home/production/cxgn/panandbase
+git clone --quiet https://github.com/solgenomics/triticum.git /home/production/cxgn/triticum
