7e70cda7efa9620c2ef73c89c88de7fc66c4702a
Tim Parsons <tep46@cornell.edu>
2019-08-19 14:08:54 -0400

    Updating entrypoint.sh to allow for deployment mode to be defined.  Default deployment mode will use starmachine to run SGN, MODE=DEVELOPMENT will start the SGN via Catalyst.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,11 @@ sed -i s/localhost/$HOSTNAME/g /etc/slurm-llnl/slurm.conf
 /etc/init.d/munge start
 /etc/init.d/slurmctld start
 /etc/init.d/slurmd start
-/etc/init.d/sgn start
-tail -f /var/log/sgn/error.log
+
+if [ "$MODE" == "DEVELOPMENT" ]; then
+	/home/production/cxgn/sgn/bin/sgn_server.pl --fork -r -d -p 8080
+else
+  /etc/init.d/sgn start
+  tail -f /var/log/sgn/error.log
+fi
 
