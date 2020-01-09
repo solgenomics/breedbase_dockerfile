28522d768304c51d17da3ac6cd2e6dd89b915aff
Lalo Martins <lalo.martins@gmail.com>
2020-01-09 19:58:58 +0200

    write logs by default

--- tools/updater/cmdline.py
+++ tools/updater/cmdline.py
@@ -11,7 +11,7 @@ from .main import main as real_main
 parser = ArgumentParser(description='Try to pull some git repositories, and update a docker image if there were changes')
 parser.add_argument('root', metavar='DIR', default='.', nargs='?', help='Root directory containing `repos` and `Dockerfile`')
 parser.add_argument('--image-name', metavar='NAME', default='breedbase/breedbase', help='Name to tag the docker image with')
-parser.add_argument('--write-logs', metavar='DIR', help='Write log files to DIR')
+parser.add_argument('--write-logs', metavar='DIR', default='updater-logs', help='Write log files to DIR')
 parser.add_argument('-c', '--update-compose', action='store_true', help='Update running container on docker-compose')
 parser.add_argument('-f', '--force-build', action='store_true', help='Build even if no git changes detected')
 parser.add_argument('-v', '--verbose', action='store_true', help='Extra output')
