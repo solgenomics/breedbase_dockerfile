1b45a7a765b9f07942cc60df26f10ae42d5f2bc7
Lukas Mueller <lam87@cornell.edu>
2020-11-19 22:07:09 -0500

    breedbase web docker should not try to start postgres.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ sed -i s/localhost/$HOSTNAME/g /etc/slurm-llnl/slurm.conf
 /etc/init.d/munge start
 /etc/init.d/slurmctld start
 /etc/init.d/slurmd start
-/etc/init.d/postgres start
+#/etc/init.d/postgres start
 
 if [ "$MODE" == "DEVELOPMENT" ]; then
 	/home/production/cxgn/sgn/bin/sgn_server.pl --fork -r -d -p 8080
