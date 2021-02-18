f5b5f1ec8cf06749d7ad44752d957e0f5a22dc02
bellerbrock <bje24@cornell.edu>
2021-02-18 17:12:17 -0500

    simplify by using default postgres docker image and loading empty_fixture.sql viaentrypoint.sh per nathan's suggestion

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,11 @@ sed -i s/localhost/$HOSTNAME/g /etc/slurm-llnl/slurm.conf
 /etc/init.d/slurmctld start
 /etc/init.d/slurmd start
 #/etc/init.d/postgres start
+if [ $(psql -Atc 'select count(distinct table_schema) from information_schema.tables;') -eq 2 ]; then
+  psql -c "CREATE USER web_usr PASSWORD 'postgres';"
+  psql -f t/data/fixture/empty_fixture.sql
+  ( cd db && ./run_all_patches.pl -u ${PGUSER} -p "${PGPASSWORD}" -h ${PGHOST} -d ${PGDATABASE} -e janedoe )
+fi
 
 if [ "$MODE" == "DEVELOPMENT" ]; then
 	/home/production/cxgn/sgn/bin/sgn_server.pl --fork -r -d -p 8080
