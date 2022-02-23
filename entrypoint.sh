356dd8fbaf1ab53316f9faca4b3ddb69f74fc994
Lukas Mueller <lam87@cornell.edu>
2022-02-22 22:13:58 -0500

    tweak slurm start reflecting new folder structure, make dirs both owner and group writable, add umask 002.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-sed -i s/localhost/$HOSTNAME/g /etc/slurm-llnl/slurm.conf
+sed -i s/localhost/$HOSTNAME/g /etc/slurm/slurm.conf
 /etc/init.d/postfix start
 /etc/init.d/cron start
+chown 106 /etc/munge/munge.key
 /etc/init.d/munge start
 /etc/init.d/slurmctld start
 /etc/init.d/slurmd start
@@ -11,9 +12,10 @@ if [ "${MODE}" = 'TESTING' ]; then
     exec perl t/test_fixture.pl --carpalways -v "${@}"
 fi
 
+umask 002
 # load empty fixture and run any missing patches
 
-if [ $(psql -Atc 'select count(distinct table_schema) from information_schema.tables;') -eq 2 ]; then
+if [ $(psql -h breedbase_db -U postgres -Atc 'select count(distinct table_schema) from information_schema.tables;') == "2" ]; then
     psql -c "CREATE USER web_usr PASSWORD 'postgres';"
     psql -f t/data/fixture/empty_fixture.sql
     ( cd db && ./run_all_patches.pl -u ${PGUSER} -p "${PGPASSWORD}" -h ${PGHOST} -d ${PGDATABASE} -e janedoe )
@@ -28,12 +30,14 @@ then
     then
         mkdir /home/production/volume/archive
         chown www-data /home/production/volume/archive
+	chmod 770 /home/production/volume/archive
     fi
 
     if [[ ! -e /home/production/volume/logs ]]
     then
         mkdir /home/production/volume/logs
         chown www-data /home/production/volume/logs
+	chmod 770 /home/production/volume/logs
     fi
 
     if [[ ! -e /home/production/volume/blast ]]
@@ -45,30 +49,35 @@ then
     then
         mkdir /home/production/volume/public
 	chown www-data /home/production/volume/public
+	chmod 770 /home/production/volume/public
     fi
 
     if [[ ! -e /home/production/volume/public/images ]]
     then
         mkdir /home/production/volume/public/images
         chown www-data /home/production/volume/public/images
+	chmod 770 /home/production/volume/images
     fi
 
     if [[ ! -e /home/production/volume/tmp ]]
     then
         mkdir /home/production/volume/tmp
         chown www-data /home/production/volume/tmp
+	chmod 770 /home/production/volume/tmp
     fi
 
     if [[ ! -e /home/production/volume/cache ]]
     then
         mkdir /home/production/volume/cache
         chown www-data /home/production/volume/cache
+	chmod 770 /home/production/volume/cache
     fi
 
     if [[ ! -e /home/production/volume/cluster ]]
     then
         mkdir /home/production/volume/cluster
         chown www-data /home/production/volume/cluster
+	chmod 770 /home/production/volume/cluster
     fi
     
     if [[ ! -e /home/production/volume/pgdata ]]
@@ -84,7 +93,8 @@ fi
 if [ "$MODE" == "DEVELOPMENT" ]; then
         /home/production/cxgn/sgn/bin/sgn_server.pl --fork -r -d -p 8080
 else
-  /etc/init.d/sgn start
+    /etc/init.d/sgn start
+    touch /var/log/sgn/error.log
   chmod 777 /var/log/sgn/error.log
   tail -f /var/log/sgn/error.log
 fi
